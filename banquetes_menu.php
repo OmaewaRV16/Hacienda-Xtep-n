de1242a108c0bceb86e5f372a693ae006333a520
Omar Romero <le20080950@merida.tecnm.mx>
2024-11-05 14:02:34 -0600

    listo, ahora si nuevos cambios

--- banquetes_menu.php
+++ banquetes_menu.php
@@ -4,11 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="PaginaPrincipal/stylebanq.css">
-<<<<<<< HEAD
     <link rel="stylesheet" href="PaginaPrincipal/animate.css">
     <script src="PaginaPrincipal/wow.min.js"></script>
-=======
->>>>>>> 2e2e65ea59f33af6a5b9b6aae829f9acc4fd3ba7
     <title>Reservas</title>
 </head>
 <body>
@@ -38,11 +35,7 @@
                 while ($fila = mysqli_fetch_assoc($result)) {
                     // Comprobación de ruta de imagen
                     $imagenUrl = 'login/' . htmlspecialchars($fila['imagen']);
-<<<<<<< HEAD
                     echo '<div class="wow animate__animated animate__zoomIn menu-card" style="background-image: url(\'' . $imagenUrl . '\');">';
-=======
-                    echo '<div class="menu-card" style="background-image: url(\'' . $imagenUrl . '\');">';
->>>>>>> 2e2e65ea59f33af6a5b9b6aae829f9acc4fd3ba7
                     echo '<div class="menu-content">';
                     echo '<h2>' . htmlspecialchars($fila['nombre_menu']) . '</h2>';
                     echo '<h3>' . htmlspecialchars($fila['descripcion']) . '</h3>';
@@ -59,11 +52,8 @@
     </div>
 
     <?php include "pie_pagina.php"; ?>
-<<<<<<< HEAD
     <script>
         new WOW().init();
     </script>
-=======
->>>>>>> 2e2e65ea59f33af6a5b9b6aae829f9acc4fd3ba7
 </body>
 </html>
--- haciendaxtepen (7).sql
+++ haciendaxtepen (7).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-11-2024 a las 03:40:51
+-- Tiempo de generación: 05-11-2024 a las 20:53:34
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -39,13 +39,7 @@ CREATE TABLE `banquete_menu` (
 --
 
 INSERT INTO `banquete_menu` (`id`, `nombre_menu`, `descripcion`, `imagen`) VALUES
-(3, 'Menú prueba 1', 'Prueba 1', 'images/2024-11-01-06-31-02-comida1.jpg'),
-(4, 'Menú prueba 2', 'Prueba 2', 'images/2024-11-01-06-33-05-comida2.jpg'),
-(5, 'Menú prueba 3', 'Prueba 3', 'images/2024-11-01-06-35-10-comida3.jpg'),
-(6, 'Menú prueba 4', 'Prueba 4', 'images/2024-11-01-06-35-30-comida 4.jpg'),
-(7, 'Menú prueba 5', 'Prueba 5', 'images/2024-11-01-06-36-23-comida5.jpg'),
-(8, 'Menú prueba 6', 'Este menú se compone de una selección cuidadosamente curada de platillos que reflejan la rica diversidad de la gastronomía contemporánea.', 'images/2024-11-01-08-25-30-WhatsApp Image 2024-10-07 at 4.02.50 PM.jpeg'),
-(9, 'Menú prueba 7', 'hola estoy probando', 'images/2024-11-04-01-05-10-el mas duro.jpg');
+(10, 'Menú prueba 1', 'tal', 'images/2024-11-05-06-25-16-comida 4.jpg');
 
 -- --------------------------------------------------------
 
@@ -72,16 +66,11 @@ CREATE TABLE `reservas_eventos` (
 --
 
 INSERT INTO `reservas_eventos` (`id`, `nombre`, `email`, `telefono`, `evento`, `fecha`, `invitados`, `mensaje`, `fecha_reserva`, `menu_banquete`, `estado`) VALUES
-(9, 'Edrick Leon Perez', 'ledrickon22@gmail.com', '9991939073', 'quinceanera', '2024-11-22', 80, 'wwd', '2024-10-22 14:59:53', 7, 'pendiente'),
-(15, 'Natanel ', 'joselo12@gmail.com', '9992456783', 'cumpleanos', '2024-11-30', 599, 'hahahahaha', '2024-11-01 19:14:43', 9, 'pendiente'),
-(16, 'Joaquin', 'admin@admin.com', '9998080008', 'corporativo', '2024-11-28', 888, 'bien', '2024-11-01 19:15:50', 7, 'pendiente'),
-(17, 'manuel', 'manuel.lv@gmail.com', '9998679690', 'comunion', '2024-11-03', 889, 'hjol', '2024-11-03 20:42:23', 5, 'realizada'),
-(20, 'Omar', 'jose.omar.romero@hotmail.com', '9994567857', 'comunion', '2024-12-25', 459, 'hola', '2024-11-03 23:18:48', 6, 'pendiente'),
-(21, 'jose', 'omar16.jorv@gmail.com', '9998768547', 'corporativo', '2024-12-19', 678, 'si', '2024-11-03 23:40:44', 7, 'pendiente'),
-(22, 'sdsad', 'gohohjp@kaka.com', '9994567857', 'comunion', '2024-12-27', 156, 'jajajajaja', '2024-11-03 23:42:17', 6, 'pendiente'),
-(23, 'Gerardo', 'Ger.lb@gmail.com', '9998754323', 'comunion', '2024-11-17', 425, 'hola', '2024-11-04 02:21:54', 7, 'pendiente'),
-(24, 'kevin', 'kev.lb@gmail.com', '9998755885', 'boda', '2024-12-17', 425, 'hihoohpp', '2024-11-04 02:23:30', 7, 'pendiente'),
-(25, 'Jay', 'Jay.co@gmail.com', '9991293748', 'boda', '2024-11-04', 546, 'jejeje', '2024-11-04 02:31:59', 6, 'pendiente');
+(27, 'Jay', 'Jay.co@gmail.com', '9991293748', 'corporativo', '2024-11-05', 546, 'snoqqp', '2024-11-05 18:25:30', 10, 'pendiente'),
+(28, 'Joaquin', 'joselo12@gmail.com', '9992456783', 'comunion', '2024-11-22', 78, 'pdwcdqs', '2024-11-05 18:27:32', 10, 'realizada'),
+(30, 'José Omar', 'jose16.jorv@gmail.com', '9993292792', 'corporativo', '2024-11-16', 200, 'kdcnosocpows', '2024-11-05 18:28:48', 10, 'cancelada'),
+(31, 'Kevin Ortiz', 'kevin@ortz.com', '9998653571', 'comunion', '2024-11-26', 666, 'mpspwdpdwp', '2024-11-05 18:29:56', 10, 'cancelada'),
+(32, 'Edrick Leon', 'ledrickon22@gmail.com', '9998688636', 'boda', '2024-12-22', 100, 'hola', '2024-11-05 19:07:38', 10, 'pendiente');
 
 -- --------------------------------------------------------
 
@@ -149,13 +138,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `banquete_menu`
 --
 ALTER TABLE `banquete_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `reservas_eventos`
 --
 ALTER TABLE `reservas_eventos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
@@ -171,7 +160,7 @@ ALTER TABLE `usuarios`
 -- Filtros para la tabla `reservas_eventos`
 --
 ALTER TABLE `reservas_eventos`
-  ADD CONSTRAINT `fk_menu_banquete` FOREIGN KEY (`menu_banquete`) REFERENCES `banquete_menu` (`id`);
+  ADD CONSTRAINT `fk_menu_banquete` FOREIGN KEY (`menu_banquete`) REFERENCES `banquete_menu` (`id`) ON DELETE SET NULL;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
